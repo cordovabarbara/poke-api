24d13856b9e4fb72568bd465b51564023d4485a3
cordovabarbara <aliendocordova@gmail.com>
2023-04-27 20:40:22 -0500

    create store and slices

---
+++ src/components/Home/FormNameUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FormNameUser = () => {
+  return (
+    <form>
+    <input type='text'>
+    FormNameUser
+    </input>
+    </form>
+  )
+}
+
+export default FormNameUser
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+    <h1> Pokedex</h1>
+    <h2>Hi Trainer</h2>
+    <p> Please give us your name to star</p>
+    </div>
+  )
+}
+
+export default Home
