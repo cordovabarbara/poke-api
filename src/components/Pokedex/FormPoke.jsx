1b20ca66079e8fbfcca107e8cf90be76add81543
cordovabarbara <aliendocordova@gmail.com>
2023-04-28 20:29:07 -0500

    create component pokeContainer

--- src/components/Pokedex/FormPoke.jsx
+++ src/components/Pokedex/FormPoke.jsx
@@ -1,24 +1,24 @@
 import React, { useRef } from "react";
 
-
 const FormPoke = () => {
-    const inputPoke = useRef();
+  const inputPoke = useRef();
 
 const adminSubmit = (e) => {
     e.preventDefault();
     console.log(inputPoke.current.value);
-};
+  };
 
-  return (
+return (
   <div>
     <form onSubmit={adminSubmit}>
-        <input ref={inputPoke} type="text"/>
+      <input ref={inputPoke} type="text" />
         <button>Search</button>
     </form>
-  </div>
-
-  )
-  
+      <select name="" id="">
+        <option value="allPokemos">AllPokemons</option>
+      </select>
+    </div>
+  );
 };
 
 export default FormPoke;
---
+++ src/components/Pokedex/pokeContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const pokeContainer = () => {
+  return <div className="poke__container">pokeContainer</div>;
+};
+
+export default pokeContainer;
